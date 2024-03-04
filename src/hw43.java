e714e5c795df78f786ca7cbaeab27a065f9aed25
Михаил <@zaymi0>
2024-03-05 01:32:39 +0300

    Updated

--- src/hw43.java
+++ src/hw43.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class hw43 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int x = scanner.nextInt();
+        int sum = 0;
+        if (x > 0) {
 
+            for (int i = 0; i <= x; i++) {
+                sum += i;
+            }
+
+            System.out.printf("The sum of all numbers from 0 to %d is %d", x, sum);
+
+        } else {
+            for (int i = 0; i <= x * -1; i++) {
+                sum += i;
+            }
+
+            sum = -sum;
+            System.out.printf("The sum of all numbers from 0 to %d is %d", x, sum);
+        }
     }
 }
