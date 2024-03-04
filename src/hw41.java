f67089eab43be4fb74e5f76378e81b4abd57d5ab
Михаил <@zaymi0>
2024-03-04 21:11:08 +0300

    Updated

--- src/hw41.java
+++ src/hw41.java
@@ -5,15 +5,16 @@ public class hw41 {
         System.out.print("Please enter your score: ");
         Scanner score = new Scanner(System.in);
         boolean sorted = false;
-        while (!sorted){
+
+        while (!sorted) {
             sorted = true;
             int num = score.nextInt();
+
             if (num <= 100) {
                 System.out.println("Congrats! You’ve passed the test!");
             } else {
                 System.out.print("Sorry, you’ve failed the test. Please enter your score: ");
                 sorted = false;
-
             }
         }
     }
