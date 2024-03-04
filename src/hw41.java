2f280f06137a7783f8bea1936a600def51b9657e
Михаил <@zaymi0>
2024-03-04 21:02:49 +0300

    start

--- src/hw41.java
+++ src/hw41.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class hw41 {
     public static void main(String[] args) {
+        System.out.print("Please enter your score: ");
+        Scanner score = new Scanner(System.in);
+        boolean sorted = false;
+        while (!sorted){
+            sorted = true;
+            int num = score.nextInt();
+            if (num <= 100) {
+                System.out.println("Congrats! You’ve passed the test!");
+            } else {
+                System.out.print("Sorry, you’ve failed the test. Please enter your score: ");
+                sorted = false;
 
+            }
+        }
     }
 }
