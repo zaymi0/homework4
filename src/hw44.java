f1187b1d70f748fbc1536fd7d21f1325b6ee63db
Михаил <@zaymi0>
2024-03-05 14:58:19 +0300

    Updated1

--- src/hw44.java
+++ src/hw44.java
@@ -4,23 +4,22 @@ public class hw44 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] tv = {"bt1", "bt2", "bt3", "bt4", "bt5"};
-        boolean sorted = false;
+        int num;
 
-        while (!sorted) {
+        do {
             System.out.print("Введите номер канала: ");
-            int num = scanner.nextInt();
+            num = scanner.nextInt();
 
             if (num >= 1 & num <= 5) {
-                num = num - 1;
-                System.out.println(tv[num]);
+                System.out.println(tv[num - 1]);
 
             } else if (num == 0) {
                 System.out.println("Выход");
-                sorted = true;
 
             } else {
                 System.out.println("Ошибка");
             }
-        }
+
+        } while (num != 0);
     }
 }
