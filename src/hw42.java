e0e4e812d8faf3b627c67be8fedaedc626921f6d
Михаил <@zaymi0>
2024-03-05 13:52:18 +0300

    Updated1

--- src/hw42.java
+++ src/hw42.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 
 public class hw42 {
     public static void main(String[] args) {
-        String str;
         Scanner scanner = new Scanner(System.in);
-        boolean sorted = false;
         String str1 = "Exit";
+        String str;
 
-        while (!sorted) {
+        do {
             System.out.print("Please enter something ");
             str = scanner.nextLine();
-            sorted = str1.equalsIgnoreCase(str);
-        }
+        } while (!str1.equalsIgnoreCase(str));
     }
 }
